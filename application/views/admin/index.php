aaf854be1bf811814084a4d6ea5e2e554f751a4f
pillioardhil <fillyoardhiles@gmail.com>
2022-11-27 23:47:31 +0700

    update dasboard

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -17,7 +17,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Admin Dashboard - Learnify</title>
+    <title>Admin Dashboard - Noor Course</title>
     <!-- General CSS Files -->
     <link rel="icon" href="<?= base_url('assets/') ?>img/favicon.png" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
