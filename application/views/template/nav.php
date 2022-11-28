d6c705fb32b15708ea4b3c87911aadc1f9972d4b
SidikR <rasidik.lcd@gmail.com>
2022-11-29 04:45:48 +0700

    navbar update

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -58,9 +58,41 @@
                 </div>
             </div>
         </div>
+        <header class=" py-4 px-3 navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <nav class=" navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <img class="image" src="../../assets/NC_logo.png" width="42px" style="margin: 1px;">
+          <img class="image" src="../../assets/NC_name.png"  width="10%" height="5%" style="margin: 1px;"> 
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <div class="collapse navbar-collapse">
+                <li class="Beranda">
+                  <a class="nav-link"  href="#">Beranda</a>
+                </li>
+                <li class="Kelas">
+                  <a class="nav-link" href="#">Kelas</a>
+                </li>
+                <li class="Kontak">
+                  <a class="nav-link" href="#">Kontak</a>
+                </li>
+              </div>
+              <li>
+                <a class="btn btn-primary ms-5 me-1"  href="login" role="button">Login</a>
+              </li>
+              <li>
+                <a class="btn btn-secondary ms-1 me-4 " href="register" role="button">Daftar</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</header>
 
         <div class="main_menu">
-            <nav class="navbar navbar-expand-lg navbar-light">
+            <nav class="navbar navbar-expand-lg">
                 <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <a class="navbar-brand logo_h" href="<?= base_url('welcome') ?>"><img src="<?= base_url('assets/') ?>img/NC_LOGO.png" alt=""></a>
