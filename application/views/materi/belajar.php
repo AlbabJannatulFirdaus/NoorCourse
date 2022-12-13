93fe5fa00f70c29f6b4daa01113f7fd37f2f9a08
Akbar <akbar.119140004@student.itera.ac.id>
2022-12-13 20:51:00 +0700

    Upload Update Pdf

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -122,23 +122,8 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
     </div>
     <!-- End Video Player -->
     <hr><hr>
-    <div class="container">
-        <div class="form-group ">
-		    <label for="berkas" class="col-sm-2 control-label">Upload Berkas <span class="text-danger">*</span></label>
-			    <div class="col-sm-8">
-		          <input type="file" name="berkas" id="berkas" required>
-                    <span class="help-block">Berkas harus bertipe PDF.</span>
-			    </div>
-            </div>
-        <div class="box-footer ">
-				<div class="col-sm-2">
-					<a href="<?=base_url('media');?>" <button type="submit" class="btn btn-outline-secondary">
-                        <i class="fa fa-save"></i> Simpan</button></a>
-            		</div>
-        </div>
-    </div>
     
-	
+    
 
     <!-- Start Deskripsi Materi -->
     <div class="container">
@@ -151,12 +136,24 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                         <!-- <h5 class="card-text"><?= $detail->nama_mapel; ?></h5> -->
                         <p class="card-text"> Deskripsi materi pelajaran : <br> <?= $detail->deskripsi; ?></p>
                     </div>
+                             <div class="form-group ">
+		                         <label for="berkas" class="col-sm-2  control-label">Upload Berkas <span class="text-danger">*</span></label>
+			                         <div class="col-sm-8">
+		                             <input type="file" name="berkas" id="berkas" required>
+                                     <span class="help-block">Berkas harus bertipe PDF.</span>
+			                        </div>
+                                 </div>
+                            <div class="box-footer ">
+				                <div class="col-sm-2">
+				        	        <a href="<?=base_url('media');?>" <button type="submit" class="btn btn-outline-secondary">
+                                     <i class="fa fa-save"></i> Simpan</button></a>
+            		            </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- End Deskripsi Materi -->
-
 
     <br>
 
